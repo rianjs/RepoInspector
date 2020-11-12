994e7f2de93bbe993a4b8c4cacb842712c4a5f7b
Rian Stockbower <rstockbower@gmail.com>
2020-11-12 10:02:34 -0500

    All comments being downloaded

--- RepoMan/RepoMan/Program.cs
+++ RepoMan/RepoMan/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -34,55 +35,59 @@ namespace RepoMan
 
         private static async Task Debug(GitHubClient client)
         {
+            const int number = 368;
             try
             {
                 var owner = "alex";
                 var repo = "nyt-2020-election-scraper";
-                // var pr = await client.PullRequest.Get("dotnet", "roslyn", 14645);    // returns the proper comment count
-                // var user = client.Repository.GetAllForUser("bojanrajkovic");
-                // var org = await client.Repository.GetAllForOrg("Cimpress");
-                // var alexPrs = await client.Repository.Get("alex", "nyt-2020-election-scraper");
                 var prOpts = new PullRequestRequest
                 {
-                    State = ItemStateFilter.Closed,
-                    // We should be able to do a binary search on the resultant list for the PR that has a last comment 
-                    SortProperty = PullRequestSort.Popularity,
+                    State = ItemStateFilter.Closed,    // Ignore anything that isn't settled
+                    SortProperty = PullRequestSort.Created,
+                    SortDirection = SortDirection.Ascending,
                 };
-                var closedPrs = await client.PullRequest.GetAllForRepository(owner, repo, prOpts);
-                // var derp = await client.PullRequest.Review.GetAllComments(owner, repo, closedPrs.First().Number)
-                var foo = closedPrs.First();
+                var closedPrs = (await client.PullRequest.GetAllForRepository(owner, repo, prOpts))
+                    .ToDictionary(pr => pr.Number);
 
-                var deepPrQuery = closedPrs
-                    .Take(5)
-                    .Select(pr => client.Issue.Comment.GetAllForIssue(owner, repo, pr.Number))
-                    .ToList();
+                var aggregate = new Dictionary<int, PullRequestDetails>(closedPrs.Count);
                 
-                await Task.WhenAll(deepPrQuery);
+                // https://github.com/alex/nyt-2020-election-scraper/pull/368 has comments, diff comments, and approvals with comments
+                var diffReviewComments = await client.PullRequest.ReviewComment.GetAll(owner, repo, number);
+                File.WriteAllText(CreateFullPath($"PullRequest-ReviewComment-GetAll-{number}.json"), Serialize(diffReviewComments));
 
-                var success = deepPrQuery
-                    .Where(t => t.IsCompletedSuccessfully)
-                    .Select(t => t.Result)
-                    .ToList();
+                var getAllForRepo = closedPrs[number];
+                File.WriteAllText(CreateFullPath($"PullRequest-GetAllForRepository-{number}.json"), Serialize(getAllForRepo));
                 
-                var fail = deepPrQuery
-                    .Where(t => t.IsFaulted)
-                    .ToList();
-                
+                // State transitions (APPROVED), and comments associated with them
+                var approvalSummaries = await client.PullRequest.Review.GetAll(owner, repo, number);
+                File.WriteAllText(CreateFullPath($"PullRequest-Review-GetAll-{number}.json"), Serialize(approvalSummaries));
+
+                // These are the comments on the PR in general, not associated with an approval, or with a commit, or with something in the diff
+                var generalPRComments = await client.Issue.Comment.GetAllForIssue(owner, repo, number);
+                File.WriteAllText(CreateFullPath($"Client-Issue-Comment-GetAllForIssue-{number}.json"), Serialize(generalPRComments));
+
+                var pullRequestDetails = new PullRequestDetails
+                {
+
+                };
+
+                // MISSING: Commit comments -- comments on a specific commit -- I haven't found a PR with a comment on a commit yet
+
                 // PR statistics of interest:
                 // Approval count per PR
                 // Comment count per PR
                 // Comment complexity per PR (i.e. how robust was the dialog?)
                 // Comments per line changed
-                
+
                 // Other
                 // Commits merged directly to master
-                
-                
-                var shallowResult = Path.Combine(_scratchDir, "shallow-prs.json");
-                var shallowWriteTask = File.WriteAllTextAsync(shallowResult, JsonConvert.SerializeObject(closedPrs, Formatting.Indented));
-                var successResult = Path.Combine(_scratchDir, "deep-prs.json");
-                var deepWriteTask = File.WriteAllTextAsync(successResult, JsonConvert.SerializeObject(success, Formatting.Indented));
-                await Task.WhenAll(shallowWriteTask, deepWriteTask);
+
+
+                // var shallowResult = Path.Combine(_scratchDir, "shallow-prs.json");
+                // var shallowWriteTask = File.WriteAllTextAsync(shallowResult, JsonConvert.SerializeObject(closedPrs, Formatting.Indented));
+                // var successResult = Path.Combine(_scratchDir, "deep-prs.json");
+                // var deepWriteTask = File.WriteAllTextAsync(successResult, JsonConvert.SerializeObject(success, Formatting.Indented));
+                // await Task.WhenAll(shallowWriteTask, deepWriteTask);
             }
             catch (Exception e)
             {
@@ -123,5 +128,11 @@ namespace RepoMan
             //var scratch = Path.Combine(thisRepo, "scratch");
             //return scratch;
         }
+
+        public static string Serialize(object o)
+            => JsonConvert.SerializeObject(o, Formatting.Indented);
+
+        public static string CreateFullPath(string fileName)
+            => Path.Combine(_scratchDir, fileName);
     }
 }
--- RepoMan/RepoMan/PullRequestDetails.cs
+++ RepoMan/RepoMan/PullRequestDetails.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Octokit;
 
 namespace RepoMan
 {
@@ -9,43 +11,82 @@ namespace RepoMan
         public string Url { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public DateTime PushedAt { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public DateTimeOffset PushedAt { get; set; }
         public long Size { get; set; }
         public bool IsArchived { get; set; }
     }
+    
     public class PullRequestDetails
     {
         public long Id { get; set; }
-        public string Url { get; set; }
-        public long Number { get; set; }
-        public List<Comment> Comments { get; set; }
-        public List<Comment> ReviewComments { get; set; }
+        public string HtmlUrl { get; set; }
+        public int Number { get; set; }
         public User Submitter { get; set; }
+        
+        /// <summary>
+        /// The top-level comments associated with a pull request
+        /// </summary>
+        public List<Comment> Comments { get; set; }
+        
+        /// <summary>
+        /// Comments associated with clicking the button in the approve/request changes workflow
+        /// </summary>
+        public List<Comment> ReviewComments { get; } = new List<Comment>();
+        
+        /// <summary>
+        /// Comments on specific parts of the diff
+        /// </summary>
+        public List<Comment> DiffComments { get; } = new List<Comment>();
+        
+        /// <summary>
+        /// Comments on specific commits
+        /// </summary>
+        public List<Comment> CommitComments { get; } = new List<Comment>();
         
         /// <summary>
         /// Open, closed, merged, etc.
         /// </summary>
         public string State { get; set; }
         
-        public DateTime OpenTimestamp { get; set; }
-        public DateTime CloseTimestamp { get; set; }
-        public DateTime MergeTimestamp { get; set; }
+        public DateTimeOffset OpenTimestamp { get; set; }
+        public DateTimeOffset CloseTimestamp { get; set; }
+        public DateTimeOffset MergeTimestamp { get; set; }
+        
+        public void WithPullRequestReviewComments(IList<PullRequestReviewComment> prReviewComments)
+        {
+            var reviewCommentsQuery = prReviewComments.Select(c => new Comment
+            {
+                Id = c.Id,
+                HtmlUrl = c.HtmlUrl,
+                Text = c.Body,
+                Timestamp = c.CreatedAt,
+                UpdatedAt = c.UpdatedAt,
+                User = new User
+                {
+                    Id = c.User.Id,
+                    Login = c.User.Login,
+                },
+            });
+            
+            ReviewComments.AddRange(reviewCommentsQuery);
+        }
     }
 
     public class Comment
     {
         public long Id { get; set; }
         public User User { get; set; }
-        public DateTime Timestamp { get; set; }
+        public string HtmlUrl { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
         public string Text { get; set; }
     }
 
     public class User
     {
         public long Id { get; set; }
-        public string Username { get; set; }
-        public string DisplayName { get; set; }
+        public string Login { get; set; }
     }
 }
